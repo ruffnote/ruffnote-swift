0a072813e98bf4a93e8be634fb1f1af94d2001e9
tnantoka <tnantoka@bornneet.com>
2014-09-03 08:32:27 +0900

    Initial Commit

---
+++ RuffnoteSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RuffnoteSwift
+//
+//  Created by Tatsuya Tobioka on 2014/09/03.
+//  Copyright (c) 2014年 Tatsuya Tobioka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
