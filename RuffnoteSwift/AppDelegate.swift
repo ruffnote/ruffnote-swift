2bd6ab9f4c35d7fb4c5b4cdc24168282259fa848
tnantoka <tnantoka@bornneet.com>
2014-09-04 14:08:19 +0900

    Add HomeViewController

--- RuffnoteSwift/AppDelegate.swift
+++ RuffnoteSwift/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        let homeController = HomeViewController()
+        let navController = UINavigationController(rootViewController: homeController)
+        
+        self.window!.rootViewController = navController
+
+        self.window!.makeKeyAndVisible()
+        
         return true
     }
 
